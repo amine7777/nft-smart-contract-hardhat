459298ff73c348b40f54cc8ebbfdb23a91b87962
Amine Kahlaoui <amine.tity@live.fr>
2022-03-19 16:51:15 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,11 +12,11 @@ const etherscanKey = process.env.ETHERSCAN_KEY;
 module.exports = {
   solidity: {
     version: "0.8.10",
-  },
+  }/*,
   networks: {
     loclahost: {
       url: "http://127.0.0.1:8545/",
-    }/*,
+    },
     rinkeby: {
       url: endpoint,
       accounts: [privateKey],
