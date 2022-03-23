a69faf3dcb087657297637fd0ee7caf5efcedb23
Amine Kahlaoui <amine.tity@live.fr>
2022-03-23 09:43:31 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,7 +4,7 @@
 
 require('@nomiclabs/hardhat-ethers');
 
-const privateKey = process.env.PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const endpoint = process.env.URL;
 const etherscanKey = process.env.ETHERSCAN_KEY;
 
@@ -12,14 +12,14 @@ const etherscanKey = process.env.ETHERSCAN_KEY;
 module.exports = {
   solidity: {
     version: "0.8.10",
-  }/*,
+  },
   networks: {
     loclahost: {
       url: "http://127.0.0.1:8545/",
     },
     rinkeby: {
       url: endpoint,
-      accounts: [privateKey],
+      accounts: [`0x${PRIVATE_KEY}`],
     }
   },
   etherscan: {
